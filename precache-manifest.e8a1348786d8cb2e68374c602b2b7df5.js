43c9765c220fe0f89f56f0cf9ed1471e669e503a
Marvin <marvincyan@gmail.com>
2018-12-17 15:53:28 -0800

    Updates

---
+++ precache-manifest.e8a1348786d8cb2e68374c602b2b7df5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dc781fe66baf0e0806b1b3d2e55a23f",
+    "url": "/my-resume-react-redux/static/media/bg.8dc781fe.jpg"
+  },
+  {
+    "revision": "28e32c1a9443d9175d16156c20a8da95",
+    "url": "/my-resume-react-redux/static/media/MarvinYanResumeNoRef.28e32c1a.pdf"
+  },
+  {
+    "revision": "38fe1a6eaa486db3e959",
+    "url": "/my-resume-react-redux/static/js/runtime~main.38fe1a6e.js"
+  },
+  {
+    "revision": "290b0d064ca0f253389e",
+    "url": "/my-resume-react-redux/static/js/main.290b0d06.chunk.js"
+  },
+  {
+    "revision": "110cb46f17960e827bba",
+    "url": "/my-resume-react-redux/static/js/1.110cb46f.chunk.js"
+  },
+  {
+    "revision": "290b0d064ca0f253389e",
+    "url": "/my-resume-react-redux/static/css/main.da9c81e9.chunk.css"
+  },
+  {
+    "revision": "110cb46f17960e827bba",
+    "url": "/my-resume-react-redux/static/css/1.1a063261.chunk.css"
+  },
+  {
+    "revision": "89934697da25bb63a970e8bb0cb7dae9",
+    "url": "/my-resume-react-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12a5ac44aa6c09bcec686e0afa28dff0.js"
+  "/my-resume-react-redux/precache-manifest.e8a1348786d8cb2e68374c602b2b7df5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-resume-react-redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
